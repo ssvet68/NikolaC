cc233919ffbec65ddf365fe910994aa74b747c12
Niki9526 <n01591561@humber.ca>
2026-01-16 15:25:03 -0500

    added to cstr.h, citation section

--- workshops_Labs/WS01/src/cstr.h
+++ workshops_Labs/WS01/src/cstr.h
@@ -1,5 +1,28 @@
 // Header File
 //cstr.h
+/* Citation and Sources...
+-----------------------------------------------------------
+OOP244 Workshop #1: cstr module HEADER FILE
+Module: cstr
+Filename: cstr.h
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027369149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date        Reason
+V0.1    2025/01/13  Created file included header file,cstr.h cstr.h created namespace seneca
+V0.2    2025/01/13  copyed string functions sigurtures from git to be used here
+V0.3    2025/01/14  Complied the file, Tested them got right output
+V0.4    2025/01/16  Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
 #ifndef SENECA_CSTR_H
 #define SENECA_CSTR_H
 #include <cstddef> 
